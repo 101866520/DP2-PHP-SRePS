d7e976c6bac2176f663c85a7e9baf8b390cf6e71
101811258 <37164341+101811258@users.noreply.github.com>
2018-09-11 12:38:53 +1000

    Add files via upload
    I have uploaded classes and methods 
    just a draft have not started working on them yet, 
    each class has it's description, so does method

---
+++ PHP-SRePS/Sales.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace PHP_SRePS
+{
+    //
+    /// <summary>
+    /// Sales. class
+    /// will handle all the user inputs
+    /// </summary>
+    public class Sales
+    {
+        private Item itemList;
+        public Sales()
+        {
+            DisPlayForm();
+        }
+        //
+        // THIS method will be used to display form 
+        // to user to enter item details.
+        //
+        private void DisPlayForm(){
+            
+        }
+        //
+        // this will be used to ADD items 
+        private void AddItem(){
+            
+        }
+        //
+        // this will be used to reset form 
+        public void ResetForm(){
+            
+        }
+        //
+        // this method will be used to 
+        // validate form, all the inputs will be checked
+        private bool ValidateForm(){
+            return true;
+        }
+        //
+        // this method will add items to list 
+        // it will use Validate Form method to
+        // to validate form and Add Item method to add items
+        // to list
+        public void AddItemsTOList(){
+            
+        }
+    }
+}
